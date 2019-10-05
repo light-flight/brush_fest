37a8b161070138952858963f456ec650a6049c66
Georgy Yuriev <georgy.yuriev@gmail.com>
2019-10-05 16:29:14 +0300

    added vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '@plugins/vuetify'
   ],
   /*
   ** Nuxt.js dev-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>Brush Dance Festival</h1>
+    <v-color-picker></v-color-picker>
   </div>
 </template>
 
