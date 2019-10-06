d4fe70a035511fea79898ddb48f2f9ccffc7d895
Georgy Yuriev <georgy.yuriev@gmail.com>
2019-10-07 02:21:44 +0300

    footer

---
+++ components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="white--text text-center footer-card"
+        color="#6B4791"
+      >
+
+        <v-card-text>
+          <v-btn outlined class="mx-4 white--text">
+            <v-icon left link href="https://www.instagram.com/brush_fest/" target="_blank">mdi-instagram</v-icon>
+            Instagram
+          </v-btn>
+          <v-btn light color="#F8EC48" link href="https://forms.gle/UKpj82JGUBMz9Kvd6" target="blank">
+            Отправить заявку
+          </v-btn>
+        </v-card-text>
+
+        <v-btn-toggle background-color="transparent" rounded>
+          <v-btn
+            v-for="(item, i) in items"
+            :key="i"
+            link
+            :href="item.link"
+            class="px-6"
+          >
+            <v-icon v-text="item.icon" left></v-icon>
+            {{ item.text }}
+          </v-btn>
+        </v-btn-toggle>
+
+        <v-card-text class="white--text pt-0">
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>BRUSH</strong> Танцевальный фестиваль
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { text: '8(903)798-41-02', icon: 'mdi-phone', link: 'tel:+79037984102' },
+      { text: 'kopytnikoff@yandex.ru', icon: 'mdi-email', link: 'mailto:kopytnikoff@yandex.ru' },
+    ],
+  }),
+}
+</script>
+
+<style lang="scss">
+  .footer-card {
+    width: 100%;
+  }
+  .contacts-links {
+    display: inline-block;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,42 @@
 <template>
-  <div>
-    <navbar />
+  <div class="container">
+    <h1>Предстоящие события</h1>
+    <v-sheet class="event" elevation='5'>
+      <img src="poster.jpg" alt="" class="poster">
+      <div class="info">
+        <ul class="info-list">
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolore unde quisquam fugiat magnam molestiae quam nihil officia, dignissimos consequatur debitis quis veritatis dolorum ab necessitatibus dolores odit culpa esse!</li>
+          <li></li>
+          <li></li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, corporis adipisci qui sed dicta et sint. Architecto quisquam voluptates fugiat modi id eaque vero explicabo, atque, expedita hic nesciunt reiciendis!</li>
+          <li></li>
+        </ul>
+      </div>
+      <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2242.534533899662!2d37.67041031552495!3d55.80131999571701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b535be0f476dd1%3A0x79e01f70b9c2483f!2zNC3QuSDQm9GD0YfQtdCy0L7QuSDQv9GA0L7RgdC10LosIDQsINCc0L7RgdC60LLQsCwgMTA3MDE0!5e0!3m2!1sru!2sru!4v1570398421926!5m2!1sru!2sru" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      </div>
+    </v-sheet>
   </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue';
-
-export default {
-  components: {
-    Navbar
+  export default {
   }
-}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  h1 {
+    margin-top: .75em;
+    margin-bottom: .5em;
+  }
+  .poster {
+    display: block;
+    width: 100%;
+  }
+  .map {
+    iframe {
+      width: 100%;
+      min-height: 400px;
+    }
+  }
 </style>
